e0e9b9735f88cb1286edd6148c6e41604ef56847
Arunsp03 <arun2002362002@gmail.com>
2024-06-22 21:04:47 +0530

    added message queue

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,3 @@
-const { log } = require("console");
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
@@ -14,12 +13,28 @@ io.on("connection", (socket) => {
        const {username}=data;
        console.log("username",username);
         console.log("socket id",socket.id);
-        users.push({"username":username,"socket":socket.id});
-        io.emit("userlist",users);
+        const existingUserIndex = users.findIndex(user => user.username === username);
+        
+        if (existingUserIndex !== -1) {
+            
+            users[existingUserIndex].socket = socket.id;
+            console.log("User already exists, updated socket ID");
+        } else {
+           
+            users.push({ "username": username, "socket": socket.id });
+            console.log("New user added");
+        }
+
+        io.emit("userlist", users);
         
     })
     socket.on("userlist",()=>{
        socket.emit("userlist",users);
+     })
+     socket.on("message",(data)=>{
+        const {sender,receiver,message,receivername}=data;
+        console.log(receiver);
+        socket.to(receiver).emit("message",{sender:sender,receivername,message});
      })
 });
 
--- Frontend/app.js
+++ Frontend/app.js
@@ -1,9 +1,73 @@
+let userlist=[];
+let messagequeue=[];
+const username=sessionStorage.getItem("username");
 const socket = io("http://localhost:3000");
 socket.emit("connected",{
-    "username":localStorage.getItem("username")
+    "username":sessionStorage.getItem("username")
 });
 socket.emit("userlist");
-socket.on("userlist",(data)=>{
-    console.log("uer entered");
-    console.log(data);
+socket.on("message",(data)=>{
+    const {sender,receivername,message}=data;
+    messagequeue.push({sender,receivername,message});   
+    const element=document.querySelector(".selected")
+    if (element){
+    let msg=[];
+    messagequeue.map((message)=>{
+    
+        if(message.sender.toString()==element.innerHTML.toString())
+            {
+                msg.push(message);
+            }
+    })
+    console.log("messages",msg);
+}
 })
+socket.on("userlist",(data)=>{
+    userlist=data;
+    if(userlist){
+        document.querySelector(".users").innerHTML='';
+        userlist.forEach(data => {
+        let element=document.createElement("p");
+        element.classList.add("username")
+        element.id=userlist.indexOf(data);
+        element.innerHTML=data.username;
+        element.addEventListener("click",()=>{
+            document.querySelector(".message-section").innerHTML='';
+            if(element.classList.contains("selected"))
+                {
+                   
+                    element.classList.remove("selected");
+                }
+                else{
+                    const existingelem=document.querySelector(".selected")
+                    if(existingelem){
+                    existingelem.classList.remove("selected");
+                    }
+                    element.classList.add("selected");
+                    let msg=[];
+                    messagequeue.map((message)=>{
+                    
+                        if(message.sender.toString()==element.innerHTML.toString())
+                            {
+                                msg.push(message);
+                            }
+                    })
+                    console.log("messages",msg);
+                    // console.log(messages);
+                    // console.log(messagequeue);
+                }
+            
+        })
+        document.querySelector(".users").append(element);
+    });
+}
+})
+function sendmsg()
+{
+    const receiver=document.querySelector(".selected");
+    const receiverId=receiver.id;
+    const message=document.getElementById('msg-input').value;
+    socket.emit("message",{sender:username,receiver:userlist[receiverId].socket,receivername:userlist[receiverId].username,message:message})
+   
+   
+}
